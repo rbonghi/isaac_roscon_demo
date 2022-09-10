12eba8c23bc9c2f4301977ab5c2d52d7601c4b71
Raffaello Bonghi <raffaello@rnext.it>
2022-09-10 14:52:59 +0100

    Realsense improvements

--- isaac_roscon_demo/launch/realsense.launch.py
+++ isaac_roscon_demo/launch/realsense.launch.py
@@ -31,7 +31,6 @@ import tempfile
 def generate_launch_description():
     pkg_rosbridge_server = get_package_share_directory('rosbridge_server')
     pkg_isaac_ros_ess = get_package_share_directory('isaac_ros_ess')
-    pkg_realsense = get_package_share_directory('realsense2_camera')
     """Launch file which brings up visual odometry node configured for RealSense."""
     realsense_camera_node = Node(
         package='realsense2_camera',
@@ -45,12 +44,11 @@ def generate_launch_description():
                 'infra_fps': 90.0,
                 'unite_imu_method': 'linear_interpolation' # copy | linear_interpolation
         }],
+        remappings=[
+            ('infra1/image_rect_raw', 'infra1/image_rect_raw_mono' ),
+            ('infra2/image_rect_raw', 'infra2/image_rect_raw_mono' )
+        ]
         )
-
-    realsense_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [pkg_realsense, '/launch/rs_launch.py'])
-    )
 
     engine_file_path = "/workspaces/isaac_ros-dev/src/isaac_ros_dnn_stereo_disparity/resources/ess.engine"
 
@@ -58,6 +56,30 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             [pkg_isaac_ros_ess, '/isaac_ros_ess.launch.py']),
         launch_arguments={'engine_file_path': engine_file_path}.items()
+    )
+
+    image_format_converter_node_left = ComposableNode(
+        package='isaac_ros_image_proc',
+        plugin='nvidia::isaac_ros::image_proc::ImageFormatConverterNode',
+        name='image_format_node_left',
+        parameters=[{
+                'encoding_desired': 'rgb8',
+        }],
+        remappings=[
+        ('image_raw', 'infra1/image_rect_raw_mono'),
+        ('image', 'infra1/image_rect_raw')]
+    )
+
+    image_format_converter_node_right = ComposableNode(
+        package='isaac_ros_image_proc',
+        plugin='nvidia::isaac_ros::image_proc::ImageFormatConverterNode',
+        name='image_format_node_right',
+        parameters=[{
+                'encoding_desired': 'rgb8',
+        }],
+        remappings=[
+        ('image_raw', 'infra2/image_rect_raw_mono'),
+        ('image', 'infra2/image_rect_raw')]
     )
 
     disparity_node = ComposableNode(
@@ -78,7 +100,7 @@ def generate_launch_description():
         namespace='disparity',
         package='rclcpp_components',
         executable='component_container_mt',
-        composable_node_descriptions=[disparity_node],
+        composable_node_descriptions=[disparity_node, image_format_converter_node_left, image_format_converter_node_right],
         output='screen'
     )
 
@@ -87,11 +109,4 @@ def generate_launch_description():
     #        [pkg_rosbridge_server, '/launch/rosbridge_websocket_launch.xml'])
     #)
 
-#        remappings=[
-#            ('left/camera_info', 'infra1/camera_info'),
-#            ('left/image_rect', 'infra1/image_rect_raw'),
-#            ('right/camera_info', 'infra2/camera_info'),
-#            ('right/image_rect', 'infra2/image_rect_raw')
-#        ]
-
-    return launch.LaunchDescription([realsense_launch, container])
+    return launch.LaunchDescription([realsense_camera_node, container])
