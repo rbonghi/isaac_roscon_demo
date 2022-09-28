44301beabff4287e1dd3dd38dd64bb38913f1307
Raffaello Bonghi <raffaello@rnext.it>
2022-09-28 09:07:47 +0100

    Load all nodes

--- isaac_roscon_demo/launch/demo.launch.py
+++ isaac_roscon_demo/launch/demo.launch.py
@@ -105,7 +105,7 @@ def generate_launch_description():
         namespace='isaac_ros',
         package='rclcpp_components',
         executable='component_container_mt',
-        composable_node_descriptions=[image_format_converter_node_left, image_format_converter_node_right],
+        composable_node_descriptions=[image_format_converter_node_left, image_format_converter_node_right, disparity_node],
         output='screen'
     )
 
@@ -126,7 +126,7 @@ def generate_launch_description():
     ld.add_action(realsense_camera_node)
     ld.add_action(container)
     #ld.add_action(rosbridge_process)
-    #ld.add_action(ess_visualizer_node)
+    ld.add_action(ess_visualizer_node)
 
     return ld
 # EOF
